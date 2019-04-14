89e4f7a846831b0a9d9b508c0bab4f404857682d
Peter <peterdierx@gmail.com>
2019-04-14 19:04:38 +0200

    about component info change

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='box'>
     <h5 class='title is-5 has-text-primary'>About Vue Playground</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p>Just a sandbox to play with Vue, Vue Router, Vuex and Bulma.</p>
   </div>
 </template>
 
