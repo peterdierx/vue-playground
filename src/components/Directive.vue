da4b92831ffb49373000f8c43e3a9e02231a9e1d
Peter <peterdierx@gmail.com>
2019-05-22 14:00:57 +0200

    vue updates

--- src/components/Directive.vue
+++ src/components/Directive.vue
@@ -2,12 +2,18 @@
   <div class='box'>
     <h5 class='title is-5'>Directives</h5>
     <p>v-focus</p>
-    <input v-focus type='text'>
+    <input v-focus type='text' v-model='input'>
+    {{ input }}
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Directive'
+    name: 'Directive',
+    data() {
+      return {
+        input: null
+      }
+    }
   }
 </script>
